b24a3e60fc04867579b365b87189b922c8a9b912
ivo liondov <ivoliondov@Sanctuary.local>
2025-12-29 07:12:41 +0000

    Remove package version as variable

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.11.4"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/approov/approov-ios-sdk.git", from: approovSDKVersion)
+        .package(url: "https://github.com/approov/approov-ios-sdk.git", from: "3.5.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
