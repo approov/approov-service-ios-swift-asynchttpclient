549a20c87999c175a8c42f49deb0431228dbcad3
ivo liondov <ivoliondov@Sanctuary.local>
2025-12-29 10:03:23 +0000

    Remove Approov target from arrays

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ApproovAsyncHTTPClient",
-            targets: ["ApproovAsyncHTTPClient", "Approov"])
+            targets: ["ApproovAsyncHTTPClient"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
