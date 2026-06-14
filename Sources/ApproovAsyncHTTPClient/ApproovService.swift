de1da4aaeb87f211e6741664c74db747f072d46b
Ivo Liondov <ivo.liondov@approov.io>
2026-06-14 09:19:49 +0100

    Use dev placeholder in setUserProperty and replace it on GHA release tag step

--- Sources/ApproovAsyncHTTPClient/ApproovService.swift
+++ Sources/ApproovAsyncHTTPClient/ApproovService.swift
@@ -204,7 +204,7 @@ public class ApproovService {
 
             approovSDKInitialised = true
             if isEnabled {
-                Approov.setUserProperty("approov-service-asynchttpclient/3.5.4")
+                Approov.setUserProperty("approov-service-asynchttpclient/dev")
             }
             TLSConfiguration.setVerifyPinningBlock(newValue: ApproovPinningVerifier.verifyPinning)
         }
